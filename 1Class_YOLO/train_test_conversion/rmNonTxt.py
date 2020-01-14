027ffc2082ae06fb43cf78fe33348adc81aa5894
Anabelle <bellech88@gmail.com>
2020-01-14 13:48:57 -0500

    updating foldr

--- 1Class_YOLO/train_test_conversion/rmNonTxt.py
+++
@@ -1,9 +0,0 @@
-import glob
-import os
-
-all_txt = glob.glob("*.txt")
-
-for file in glob.glob("*.jpeg"):
-    txt_name = file[:-5] + ".txt"
-    if txt_name not in all_txt:
-        os.remove(file)
